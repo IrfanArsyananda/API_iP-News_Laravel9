42cf870de8676213c6b3a97a353464fd9c2b569d
Irfan Arsyananda <71767382+IrfanArsyananda@users.noreply.github.com>
2024-01-15 23:47:24 +0700

    API Resource (Collection)

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{slug}', [PostController::class, 'show']);
+Route::get('/posts', [PostController::class, 'index'])->middleware(['auth:sanctum']);
+Route::get('/posts/{slug}', [PostController::class, 'show'])->middleware(['auth:sanctum']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
